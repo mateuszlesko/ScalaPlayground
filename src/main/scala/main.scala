c889f9ad028f1b1bc96efb3e895495c5d4dcb04a
mateuszlesko <mateusz.lesko@outlook.com>
2022-03-02 21:27:58 +0100

    learn some basics

---
+++ src/main/scala/main.scala
@@ -0,0 +1,13 @@
+/*
+Traits = interfaces, share fileds between classes. Classes and objects can extends traits but traits can not be initalized so have no parameters
+*/
+import Scala1._
+object Main {
+  
+    
+  def main(args: Array[String]): Unit = {
+     play()
+  }
+ 
+  
+}
---
+++ src/main/scala/scala1.scala
@@ -0,0 +1,38 @@
+/*
+Traits = interfaces, share fileds between classes. Classes and objects can extends traits but traits can not be initalized so have no parameters
+*/
+object Scala1 {
+  
+  trait Animal{
+    def speak():Unit
+  }
+  
+  trait HasTail{
+    def wagTail():Unit
+  }
+  
+  class Dog extends Animal{
+    def speak() = print("Hau")
+    //def wagTail() = println("⎞⎜⎛  ⎞⎜⎛")
+  }
+    
+  def play(): Unit = {
+      println("Hello world!")
+      val numbers = List(1,2,3,4)
+      val newNumbers = numbers.map(i => i*2)
+      val newNumbers2 = newNumbers.map(_*2)
+      println(newNumbers2)
+      val elemGt4 = newNumbers2.filter( num => num > 4)
+      println(elemGt4)
+      val firstGt2 = newNumbers2.find(_ >= 2) //take first element which condition is true
+      println(firstGt2)
+      val elemsGt3 = newNumbers2.takeWhile(_ > 3)
+      println(elemsGt3)
+      val newNumbers3 = newNumbers2.takeWhile(_ < 7).map(_ + 1)
+      println(newNumbers3)  
+      val doggy = new Dog()
+      doggy.speak()
+  }
+ 
+  
+}
